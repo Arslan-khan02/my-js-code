322bdb48bd7e23fed1e876de2713143995a02054
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-18 12:34:13 +0000

    convertion on string,num,boolean

---
+++ convertion_and_operatiion/convertion.js
@@ -0,0 +1,31 @@
+// let score = "ddd"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "50" --> 50
+// "50ddd" -->NaN
+// true --> 1 , fals -->0
+
+
+// convertion of boolean 
+
+let isLoggedIn =0;
+let kkk = Boolean(isLoggedIn)
+
+// console.log(typeof kkk);
+
+
+// 1=> true ; 0=> false 
+// " " => false 
+//  "string"=> true
+
+// convertion of string
+
+let name =  12
+let a = String(name)
+console.log(a);
+console.log(typeof a);
