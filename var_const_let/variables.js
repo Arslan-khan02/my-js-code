eba4b797d60e02f4f6fa40d394d4faf19abec939
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-18 11:59:45 +0000

    variables

---
+++ var_const_let/variables.js
@@ -0,0 +1,11 @@
+const accId = 123455
+let email ="arslan@gmail.com"
+var password ="55550"
+email = "lov@gmail.com"
+let state;
+
+
+console.table([accId,email,password,state])
+
+
+/* prfer not use var bcause of  issue in block scope and functional scope */
