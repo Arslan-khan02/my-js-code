f50b6f0f77fc09cc468a22e3d504495193876720
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-20 10:15:54 +0000

    data type summary

---
+++ datatype_summary/datatype.js
@@ -0,0 +1,31 @@
+// primitiv data types (call by value)
+// 7types -> string,number,boolean,null,undifiend,symbols,bigInt.
+// let name ="koibhi"
+// let sum =555
+// let isLoggedIn = true
+// let temp = null
+// let email;
+// let userId = Symbol('abc123')
+// let bigNum =1234564587568745587n;
+// console.log(typeof email);
+
+
+
+
+
+
+// reference (non-primitive) [call by reference]
+// array,functions,objcets *typeof of all non-primitive datatype is object function*
+// const heroes =["akaka","llll","pppp"]
+// const student = function(){
+//   console.log("my world");
+//   return student;
+// }
+ 
+
+const student_id ={
+name : "pop",
+age : 12,
+}
+
+console.log(typeof student_id);
