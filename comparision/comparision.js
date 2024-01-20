123ebe59c53e65d22a042611152bbdfee3192f7c
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-20 07:55:10 +0000

    comparision and equality check

---
+++ comparision/comparision.js
@@ -0,0 +1,23 @@
+// console.log("hii");
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 <= 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1); // iske andr javascript automatically conversion krdeta hai comparision ke andr
+// ******************************************************************
+
+
+// console.log(null > 0);
+// console.log(null >= 0); 
+// console.log(null == 0);
+/* the reason is that an equality check == and comparision >< >= <= work differently.
+/* comparision converts null to a number, treating is as 0.
+/* that's y (3) null >= 0 is true and (1) null >0 is false.'
+comparision and equality check both ae diifernt */
+
+// strict check
+
+
