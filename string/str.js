d5bd8b57b25aef8eaf3bc5440128beda221af9e8
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-23 12:06:11 +0000

    methods on strings

---
+++ string/str.js
@@ -0,0 +1,21 @@
+// concetination of strings 
+
+// let a ="arslan"
+// let b= 12
+// console.log(`my name is ${a} and my age is ${b}`);
+
+// string ko declare krne ka dusra tarika 
+
+const name = new String('arslan')
+// console.log(name);
+// console.log(name[0]);//key value pair pata krne k liye
+// console.log(name.__proto__);//prototype pata krne k liye / method ke liye 
+// console.log(name.charAt(5));//character find krne ke liye 
+// console.log(name.indexOf('a'));//to find index no.
+
+// const newString = name.substring(0, 3)
+// console.log(newString);//kaha se kaha tk ka lena hai
+
+// slice method
+// let anotherString = name.slice(1, 5)
+// console.log(anotherString);
