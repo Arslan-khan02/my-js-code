84ab0b8242b27f1cddd14a29ec8ae3b32e91d9ef
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-24 08:02:11 +0000

    number and maths

---
+++ number/num.js
@@ -0,0 +1,18 @@
+// const balance = new Number(100)
+// // console.log(balance);
+// // console.log(balance.toFixed(2));//number ke aage point lg jata hai
+// // console.log(balance.toString().length);//number to string 
+// let otherNumber = 125.65
+// // console.log(otherNumber.toPrecision(3));//this method use for approx value
+
+// let hundreds = 100000
+// // console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ****************************maths*********************
+// Maths is an object
+console.log(Math);
+console.log(Math.abs(-5));//this property use for converting all the -ve value to +ve value 
+console.log(Math.round(11.6));//use for rounding number
+console.log(Math.ceil(5.44));//uper wali value lega
+console.log(Math.floor(5.44));//kam wali value lega
