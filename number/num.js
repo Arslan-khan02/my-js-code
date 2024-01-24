13bff47215c8f65447b56f87c0d5cfdec04a6bdd
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-24 11:49:15 +0000

    math

--- number/num.js
+++ number/num.js
@@ -11,8 +11,15 @@
 
 // ****************************maths*********************
 // Maths is an object
-console.log(Math);
-console.log(Math.abs(-5));//this property use for converting all the -ve value to +ve value 
-console.log(Math.round(11.6));//use for rounding number
-console.log(Math.ceil(5.44));//uper wali value lega
-console.log(Math.floor(5.44));//kam wali value lega
+// methods on Math property+++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-5));//this property use for converting all the -ve value to +ve value 
+// console.log(Math.round(11.6));//use for rounding number
+// console.log(Math.ceil(5.44));//uper wali value lega
+// console.log(Math.floor(5.44));//kam wali value lega
+// console.log(Math.min(4,5,6,1));//to find min value 
+// console.log(Math.max(55,46,4555,1));//to find max value
+// console.log(Math.random());//*gives number btw 0-1*
+const min = 50
+const max = 100
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
